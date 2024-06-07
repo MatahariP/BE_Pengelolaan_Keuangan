71c741b368cfb255ee41aa6d95f11cae12b82fe8
Adip <adip2003@gmail.com>
2024-06-07 21:09:10 +0700

    fix:  connection

--- dbConnect.js
+++ dbConnect.js
@@ -7,6 +7,10 @@ const pool = new Pool({
   database: process.env.PG_DATABASE,
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: true,
+    mode: process.env.PGSSLMODE || "prefer",
+  },
 });
 
 module.exports = {
